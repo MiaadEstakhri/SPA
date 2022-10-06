0b982f87b81614d2ce81717d8308fc869fca01e9
MiaadEstakhri <miaad8104@gmail.com>
2022-10-06 12:59:05 +0330

    not found pag(404)

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,13 @@
 import HomePage from "./pages/HomePage";
 import AboutUs from "./pages/AboutUsPage";
 import Profile from "./pages/Profile";
+import NotFound from "./pages/NotFound";
 
 const routers = [
   { path: "/", element: <HomePage />, exact: true },
   { path: "/about-us", element: <AboutUs /> },
   { path: "/profile", element: <Profile /> },
+  { path: "*", element: <NotFound /> },
 ];
 
 export default routers;
